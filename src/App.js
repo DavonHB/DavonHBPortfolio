139d11935459bd6a541ffcead74c29aca75c9481
Nosferatu <davonb113@gmail.com>
2022-06-23 20:10:19 -0500

    side nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom';
 import './App.scss';
+import Layout from './components/Layout';
 
 function App() {
   return (
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,48 @@
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import { faGithub, faLinkedin, faSlack } from '@fortawesome/free-brands-svg-icons';
+
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <div className='logo' to='/'>
+            <p className='name'>Davon</p>
+            <p className='name'>Brown</p>
+        </div>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' 
+                href="https://www.linkedin.com/in/davon-brown-911909206/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' 
+                href="https://github.com/DavonHB">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' 
+                href="/">
+                    <FontAwesomeIcon icon={faSlack} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar;
