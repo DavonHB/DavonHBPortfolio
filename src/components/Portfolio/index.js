3bb04c2b67681990f0ba6f8af7c25d11a8d31a25
Nosferatu <davonb113@gmail.com>
2022-06-25 21:57:51 -0500

    added github links to cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -20,7 +20,8 @@ const Portfolio = () => {
                             <p className="description">Codewire is a Code Repository in the form of a community blog. This app allows users to post, comment on and rate reusable snippets of code.</p>
                         </div>
                         <div className="spacing deploy">
-                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer" className="link">Click Here for the Deployed App</a>
+                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer"className="card-link">Deployed App Link</a>
+                            <a href="https://github.com/mattparker124/codewire" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
                         </div>
                     </div>
                 </div>
@@ -35,7 +36,8 @@ const Portfolio = () => {
                             <p className="description">Run-Buddy is a prop website designed to showcase my early CSS and HTML skills.</p>
                         </div>
                         <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/run-buddy/" target="_blank" rel="noreferrer" className="link">Click Here for the Deployed App</a>
+                            <a href="https://davonhb.github.io/run-buddy/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
+                            <a href="https://github.com/DavonHB/run-buddy" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
                         </div>
                     </div>
                 </div>
@@ -50,7 +52,8 @@ const Portfolio = () => {
                             <p className="description">This is a simple Weather App built from scratch designed to display the weather for today and the whole week.</p>
                         </div>
                         <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/WeatherApp/" target="_blank" rel="noreferrer" className="link">Click Here for the Deployed App</a>
+                            <a href="https://davonhb.github.io/WeatherApp/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
+                            <a href="https://github.com/DavonHB/WeatherApp" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
                         </div>
                     </div>
                 </div>
@@ -65,7 +68,8 @@ const Portfolio = () => {
                             <p className="description">Choose Your Mood is a collaborative project designed to showcase knowledge of an extensive API. This app deploys a set of songs depending on which mood you choose.</p>
                         </div>
                         <div className="spacing deploy">
-                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer" className="link">Click Here for the Deployed App</a>
+                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
+                            <a href="https://github.com/JackJohnsonGH/project_vb_01" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
                         </div>
                     </div>
                 </div>
@@ -80,7 +84,8 @@ const Portfolio = () => {
                             <p className="description">Budget Tracker uses Progressive Web Application (PWA) to enable users to add expenses and deposits to their budget with or without an online connection. Transactions are able to be entered offline and the data populates the total when back online.</p>
                         </div>
                         <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/Budget-Tracker/" target="_blank" rel="noreferrer" className="link">Click Here for the Deployed App</a>
+                            <a href="https://davonhb.github.io/Budget-Tracker/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
+                            <a href="https://github.com/DavonHB/Budget-Tracker" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
                         </div>
                     </div>
                 </div>
