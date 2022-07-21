762b62a6754be983138ccab12d109f870a28823b
Nosferatu <davonb113@gmail.com>
2022-07-21 11:40:53 -0500

    added project NetPets to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,99 +1,36 @@
 import './index.scss'
 import Loader from 'react-loaders'
+import { data } from '../../data'
+
 
 const Portfolio = () => {
     return (
-        <>
         <div className="container portfolio-page">
             <div className='text-zone'>
                 <h1>My Works</h1>
             </div>
             <div className='card-container'>
-
-                <div className="card card1">
-                    <div className="overlay">
-                        <div className = "spacing head">
-                            <p>Codewire</p>
-                            <br></br>
+                {data.map((project, i) => {
+                    return (
+                        <div className="card" key={i}>
+                            <div className="overlay" >
+                                <div className = "spacing head">
+                                    <p>{project.title}</p>
+                                    <br></br>
+                                </div>
+                                <div className = "spacing description-container">
+                                    <p className="description">{project.description}</p>
+                                </div>
+                                <div className="spacing deploy">
+                                    <a href={project.appLink} target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
+                                    <a href={project.gitLink} target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
+                                </div>
+                            </div>
                         </div>
-                        <div className = "spacing description-container">
-                            <p className="description">Codewire is a Code Repository in the form of a community blog. This app allows users to post, comment on and rate reusable snippets of code.</p>
-                        </div>
-                        <div className="spacing deploy">
-                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer"className="card-link">Deployed App Link</a>
-                            <a href="https://github.com/mattparker124/codewire" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card card2">
-                    <div className="overlay">
-                        <div className = "spacing head">
-                            <p>Run-Buddy</p>
-                            <br></br>
-                        </div>
-                        <div className = "spacing description-container">
-                            <p className="description">Run-Buddy is a prop website designed to showcase my early CSS and HTML skills.</p>
-                        </div>
-                        <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/run-buddy/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
-                            <a href="https://github.com/DavonHB/run-buddy" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card card3">
-                    <div className="overlay">
-                        <div className = "spacing head">
-                            <p>Weather App</p>
-                            <br></br>
-                        </div>
-                        <div className = "spacing description-container">
-                            <p className="description">This is a simple Weather App built from scratch designed to display the weather for today and the whole week.</p>
-                        </div>
-                        <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/WeatherApp/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
-                            <a href="https://github.com/DavonHB/WeatherApp" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card card4">
-                    <div className="overlay">
-                        <div className = "spacing head">
-                            <p>Choose Your Mood</p>
-                            <br></br>
-                        </div>
-                        <div className = "spacing description-container">
-                            <p className="description">Choose Your Mood is a collaborative project designed to showcase knowledge of an extensive API. This app deploys a set of songs depending on which mood you choose.</p>
-                        </div>
-                        <div className="spacing deploy">
-                            <a href="https://codewireapp.herokuapp.com/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
-                            <a href="https://github.com/JackJohnsonGH/project_vb_01" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card card5">
-                    <div className="overlay">
-                        <div className = "spacing head">
-                            <p>Budget-Tracker</p>
-                            <br></br>
-                        </div>
-                        <div className = "spacing description-container">
-                            <p className="description">Budget Tracker uses Progressive Web Application (PWA) to enable users to add expenses and deposits to their budget with or without an online connection. Transactions are able to be entered offline and the data populates the total when back online.</p>
-                        </div>
-                        <div className="spacing deploy">
-                            <a href="https://davonhb.github.io/Budget-Tracker/" target="_blank" rel="noreferrer" className="card-link">Deployed App Link</a>
-                            <a href="https://github.com/DavonHB/Budget-Tracker" target="_blank" rel="noreferrer" className="card-link">GitHub Repo Link</a>
-                        </div>
-                    </div>
-                </div>
-
+                    )
+                })}
             </div>
         </div>
-        <Loader type="pacman" />
-        </>
     )
 }
 
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        title: "Codewire",
+        description: "Codewire is a Code Repository in the form of a community blog. This app allows users to post, comment on and rate reusable snippets of code.",
+        image: "url('../../assets/images/codewire.png')",
+        appLink: "https://codewireapp.herokuapp.com/",
+        gitLink: "https://github.com/mattparker124/codewire",
+    },
+    {
+        title: "Run-Buddy",
+        description: "Run-Buddy is a prop website designed to showcase my early CSS and HTML skills.",
+        image: "url('../../assets/images/yellow.png')",
+        appLink: "https://davonhb.github.io/run-buddy/",
+        gitLink: "https://github.com/DavonHB/run-buddy",
+    },
+    {
+        title: "Weather App",
+        description: "This is a simple Weather App built from scratch designed to display the weather for today and the whole week.",
+        image: "url('../../assets/images/weatherapp.png')",
+        appLink: "https://davonhb.github.io/WeatherApp/",
+        gitLink: "https://github.com/DavonHB/WeatherApp",
+    },
+    {
+        title: "NetPets",
+        description: "NetPet's is a MERN app that allows you to take care of your virtual (Net) Pet.",
+        image: "url('../../assets/images/netpets.png')",
+        appLink: "",
+        gitLink: "https://github.com/GartTheSkeleton/NetPets",
+    },
+    {
+        title: "Budget Tracker",
+        description: "Budget Tracker uses Progressive Web Application (PWA) to enable users to add expenses and deposits to their budget with or without an online connection. Transactions are able to be entered offline and the data populates the total when back online.",
+        image: "url('../../assets/images/budgettracker.png')",
+        appLink: "https://davonhb.github.io/Budget-Tracker/",
+        gitLink: "https://github.com/DavonHB/Budget-Tracker",
+    }
+]
